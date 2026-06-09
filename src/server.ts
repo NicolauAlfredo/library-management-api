44a00bb0c27410c717dcfdfd0e0d2bfaad66afe9
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-09 15:58:15 +0200

    refactor: separate express app from server bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,9 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
 import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
 
-const app = express();
-
 const PORT = process.env.PORT || 8000;
-
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Library Management API is running",
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
